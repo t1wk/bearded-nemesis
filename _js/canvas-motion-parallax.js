1e06c998be6d8560a666e0002cd12f8a302137c6
T1wk <christophe.zf@gmail.com>
2014-03-10 02:52:06 +0100

    WIP - Rotation animation on scroll

--- _js/canvas-motion-parallax.js
+++ _js/canvas-motion-parallax.js
@@ -13,6 +13,26 @@
 */
 
 //=============================================================//
+// Useful function to deal with rotation degrees
+//=============================================================//
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+
+    if(angle < 0) angle +=360;
+    return angle;
+}
+
+//=============================================================//
 // Define Path for images
 //=============================================================//
 	var rootPath, sequencePath, imgPrefix;
@@ -176,9 +196,43 @@ function motionParallax(){
 					// Velocity is smoothed if you don't have the same amount of frames by sequence 
 					velocity = Math.round(((offset * 3) / totalFrames)); 
 
+					
+					// Define if the number is an even or not
+					function isEven(num) {
+					  return isNaN(num) && num !== false && num !== true ? false : num % 2 == 0;
+					}
+					
 					// Article is into the ViewPort?
 					// Great, render the sequence
-					if(article.isOnScreen()){						
+					if(article.isOnScreen()){
+						
+						el = $(canvas[i]);
+						animation = offset / 500;
+						leftPos = animation * 5;
+						size = offset * .5;
+						yDegree = 2;
+						yPos = 11;
+						yPosEven = 20;
+						
+						if(isEven(i) === true) {
+							
+							article.css({
+								//'top': + size
+								//'height' : 80 + '%' <= 120 + '%' ? 80 + size + '%' : 120 + '%',
+								//'width' : 120 + '%' <= 150 + '%' ? 120 + size + '%' : 150 + '%'
+							});
+							
+							el.css({
+								'transform': 'rotateY(' + yDegree >= -yDegree + 'deg)' ? 'rotateY(' + (yDegree - animation) + 'deg)' : 'rotateY(' + -yDegree + 'deg)',
+								'left' : yPos + '%' >= -yPos + '%' ? (yPos - leftPos) + '%' : -yPos + '%'
+							});
+						} else {
+							el.css({
+								'transform': 'rotateY(' + -yDegree <= yDegree + 'deg)' ? 'rotateY(' + (-yDegree + animation) + 'deg)' : 'rotateY(' + yDegree + 'deg)',
+								'left' : -yPosEven + '%' <= yPosEven + '%' ? (-yPosEven + leftPos) + '%' : yPosEven + '%'
+							});
+						}
+						
 						render = (function(value){
 							return function(){
 							
